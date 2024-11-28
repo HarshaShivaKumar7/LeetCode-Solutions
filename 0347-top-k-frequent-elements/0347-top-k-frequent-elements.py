d4a9664f86013ebc0a9d99ac4f8f342cbcd2413e
Harsha ShivaKumar <54899429+HarshaShivaKumar7@users.noreply.github.com>
2024-11-28 10:19:13 +0530

    Time: 0 ms (100%), Space: 20.7 MB (50.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,21 +5,15 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counts = Counter(nums)
-        min_heap=[]
-        print(counts)
-        print(counts.items())
+        min_heap = []
         for element, freq in counts.items():
             heappush(min_heap, (freq, element))
-            if len(min_heap)>k:
+            if len(min_heap) > k:
                 heappop(min_heap)
-                print("Min Heap after Pop")
-                print(min_heap)
         return [num for (count, num) in min_heap]
-
 
         # k=2
 
         # 1-3
         # 2-2
         # 3-1
-   
